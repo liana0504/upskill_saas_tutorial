1d9df1e0961bf5b61aba704b730a7c0599c7d38f
Liana Gilliam <lianagilliam@gmail.com>
2019-03-18 00:37:37 +0000

    Added create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,19 @@
 class ContactsController < ApplicationController
   def new
-    
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Thankyou for your feedback!! We'll chat soon."
+    else
+      redirect_to new_contact_path, notice: "Opps. Something goofed. Let's try that again.."
+    end
+  end
+  
+  private
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+      end
 end
