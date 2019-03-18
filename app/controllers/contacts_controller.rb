ea02284ce01428a507b75d6636c69221e5e26fe2
Liana Gilliam <lianagilliam@gmail.com>
2019-03-18 16:52:49 +0000

    Best save practice

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,6 +14,6 @@ class ContactsController < ApplicationController
   
   private
     def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
-      end
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
